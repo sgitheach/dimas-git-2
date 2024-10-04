a55dfec744dafec432f2b1a8a6804603c547c46d
sgitheach <fadillahkusumawardana21@gmail.com>
2024-10-04 09:29:26 +0700

    menambah paragraf

--- dimas-git-2/index.php
+++ dimas-git-2/index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
         <h1>Hello dunia</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium voluptates fugit possimus et iure, incidunt debitis vitae libero eaque voluptatum nostrum, autem vero minima natus temporibus aut dolorem a quae!</p>
 </body>
 </html>
